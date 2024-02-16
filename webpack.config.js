c4b57931f30f83b51dc6914e24c44cdce53b342a
VijaySingh8650 <rathorevijay995@gmail.com>
2024-02-16 22:36:02 +0530

    build-done

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     publicPath: '/',
   },
   plugins: [
